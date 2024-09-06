225edf9a6ab21082a801521c8e2732e50cec5ad0
marcelobenigno <marcelobenigno123@gmail.com>
2024-09-06 17:17:36 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,16 @@
+let dados = [
+    {
+    titulo: "vasco",
+    descricao: "O Vasco da Gama conquistou uma importante vitória ao superar o Vitória em pleno Barradão pela 25ª rodada do Campeonato Brasileiro de 2024.",
+    link: "https://www.lance.com.br/vasco/vasco-vence-o-vitoria-fora-de-casa-e-encosta-no-pelotao-de-cima-no-brasile,irao.html",
+    tags:"gama brasileiro "
+},
+
+   { titulo: "internacional",
+    descricao:"O Internacional conquistou uma importante vitória ao superar o Juventude em pleno Alfredo Jaconi pela 25ª rodada do Campeonato Brasileiro de 2024.",
+link:"https://www.internacional.com.br/noticias/cronica-juventude-x-inter-25a-rodada-brasileirao-2024",
+tags:"jaconi brasileiro campeonato"
+},
+];
+
+    
